ee11216560efd29d807ed7d01fcad822880f986b
maze <permafuture@protonmail.com>
2019-03-15 16:43:52 -0600

    added crossorigin to webfonts

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -54,10 +54,10 @@ const Layout = ({ children }) => (
   <ConfigContext.Provider>
     <GlobalStyle />
     <style type="text/css">{FONT}</style>
-    <link rel="preload" href="/mbpicturehouse_oneregular_macroman/MB_Picture_House_One_Regular-webfont.woff2" as="font" />
-    <link rel="preload" href="/mbpicturehouse_oneregular_macroman/MB_Picture_House_One_Regular-webfont.woff" as="font" />
-    <link rel="preload" href="/mbpicturehouse_oneregular_macroman/MB_Picture_House_One_Bold-webfont.woff2" as="font" />
-    <link rel="preload" href="/mbpicturehouse_oneregular_macroman/MB_Picture_House_One_Bold-webfont.woff" as="font" />
+    <link rel="preload" href="/mbpicturehouse_oneregular_macroman/MB_Picture_House_One_Regular-webfont.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="/mbpicturehouse_oneregular_macroman/MB_Picture_House_One_Regular-webfont.woff" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="/mbpicturehouse_oneregular_macroman/MB_Picture_House_One_Bold-webfont.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="/mbpicturehouse_oneregular_macroman/MB_Picture_House_One_Bold-webfont.woff" as="font" type="font/woff2" crossorigin="anonymous"/>
 
     <ConfigContext.Consumer>
       {appConfig => (
