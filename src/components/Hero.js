64eacbc2be119417610106ea2720184e9086a1ec
classifer <permafuture@protonmail.com>
2020-03-06 16:20:05 -0700

    removed the staticquery from hero, using direct url instead

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,60 +8,44 @@ const Hero = () => (
     {appConfig => (
       <ResponsiveContext.Consumer>
         {size => (
+          <Box
+            height="90vh"
+            pad="xlarge"
+            background="accent-2"
+            align="center"
+            flex="grow"
+            justify="center"
+          >
+
             <Box
-              height="90vh"
-              pad="xlarge"
-              background="accent-2"
-              align="center"
-              flex="grow"
-              justify="center"
+              width={size === 'small' ? 'small' : 'medium'}
+              height={size === 'small' ? 'small' : 'medium'}
+              margin="small"
             >
-              <StaticQuery
-                query={graphql`
-                  {
-                    imageSharp(id: {eq: "ea3ca1d0-1433-5e6d-9671-509d37f63f8a"}) {
-                      original {
-                        src
-                      }
-                    }
-                  }
-                `}
-                render={data => {
-                  console.log(data)
-                  const { src } = data.imageSharp.original
-                  return (
-                    <Box
-                      width={size === 'small' ? 'small' : 'medium'}
-                      height={size === 'small' ? 'small' : 'medium'}
-                      margin="small"
-                    >
-                      <Image fit="contain" src={src} alt="" a11yTitle="logo" />
-                    </Box>
-                  )
-                }}
-              />
-
-              <Heading
-                color="brand"
-                size="xlarge"
-                level="1"
-                textAlign="center"
-                a11yTitle="Application title"
-                margin="small"
-              >
-                <Anchor href="/">{appConfig.title}</Anchor>
-              </Heading>
-
-              <Heading
-                color="secondary"
-                textAlign="center"
-                a11yTitle="Application sub title"
-                level="2"
-                size="large"
-              >
-                Nob Hill's home-grown family-owned bookstore
-              </Heading>
+              <Image fit="contain" src="/static/icon-74681f5c464fedee6e10813f9fd14965.png" alt="" a11yTitle="logo" />
             </Box>
+
+            <Heading
+              color="brand"
+              size="xlarge"
+              level="1"
+              textAlign="center"
+              a11yTitle="Application title"
+              margin="small"
+            >
+              <Anchor href="/">{appConfig.title}</Anchor>
+            </Heading>
+
+            <Heading
+              color="secondary"
+              textAlign="center"
+              a11yTitle="Application sub title"
+              level="2"
+              size="large"
+            >
+                Nob Hill's home-grown family-owned bookstore
+            </Heading>
+          </Box>
         )}
       </ResponsiveContext.Consumer>
     )}
