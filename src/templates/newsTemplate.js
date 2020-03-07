542343f697b732b7f4463ad90c36fbe8300f58a8
classifer <permafuture@protonmail.com>
2020-03-06 18:52:52 -0700

    styled News template

--- src/templates/newsTemplate.js
+++ src/templates/newsTemplate.js
@@ -1,20 +1,52 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { Box, Heading, Markdown, Paragraph, Text, Button } from 'grommet'
+import { LinkPrevious, LinkNext, Multiple } from 'grommet-icons'
+import Layout from '../components/PageLayout'
+import Slice from '../components/Slice'
+import SEO from '../components/SEO'
+
 
 export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="news-post-container">
-      <div className="news-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="news-post-content"
+    <Layout>
+      <Slice>
+        <Box align="end">
+        <Heading level="3" size="large" margin="small">{frontmatter.title}</Heading>
+        <Heading level="4" size="small" margin={{top:'small', bottom:'large'}}>{frontmatter.date}</Heading>
+        </Box>
+        <Box
           dangerouslySetInnerHTML={{ __html: html }}
         />
-      </div>
-    </div>
+      <Box direction="row-responsive" margin={{top:'xlarge', horizontal:'none',bottom:'none'}} alignSelf="center">
+        <Button
+          color="light-3"
+          pad="small"
+          alignSelf="center"
+          icon=<LinkPrevious />
+          label="Newer"
+          href="books"
+        />
+      <Button
+        color="light-3"
+          alignSelf="center"
+          icon=<Multiple />
+          label="Archive"
+          href="books"
+        />
+      <Button
+        color="light-3"
+
+          alignSelf="center"
+          icon=<LinkNext />
+          label="Older"
+          href="books"
+        />
+      </Box>
+    </Slice>
+    </Layout>
   )
 }
 
